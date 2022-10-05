1d1d4499dc352fb668c3c530881cfe96cc75958c
Miro <asc9602@naver.com>
2022-10-05 15:36:19 +0900

    clear

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import { graphql } from "@octokit/graphql";
+import React, {useEffect, useState} from 'react';
 
 function App() {
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true)
+  
+  async function repo() {
+    const { repository } = await graphql(
+      /* 아래는 요청할 쿼리가 들어가는 영역 */
+      `
+        {
+          repository(owner: "codestates-seb", name: "agora-states-fe") {
+            discussions(first: 20) {
+              edges {
+                node {
+                  title
+                  url
+                  author {
+                    resourcePath
+                  }
+                }
+              }
+            }
+          }
+        }
+      `,
+      {
+        headers: {
+          authorization: `token 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`,
+        },
+      }
+    );
+    return repository;
+  }
+
+  useEffect(() => {
+    repo()
+    .then(res => {
+      setData(res.discussions.edges)
+      setIsLoading(false)
+    })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isLoading? "loading..." : data.map((el, index) => {
+        return <li key = {index}>
+          <a href={el.node.url}>{el.node.title}</a>
+          <span>{el.node.author.resourcePath}</span>
+        </li>
+      })}
     </div>
   );
 }
